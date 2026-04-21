877c4fdc88ea63bf91050fefb4701bbc731b1a46
chloeleejx <leejingxichloe@gmail.com>
2026-04-21 09:49:47 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css"; // Optional, but good to have
 import App from "./App";
-import { Analytics } from "@vercel/analytics/react";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -10,6 +9,5 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
-    <Analytics />
   </StrictMode>
 );
